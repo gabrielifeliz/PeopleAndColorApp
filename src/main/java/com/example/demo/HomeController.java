a4a830a52b2a63b8d5bef7c003f0b99afb7921ab
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-06-29 10:40:44 -0400

    updates

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -19,26 +21,34 @@ public class HomeController {
     @RequestMapping("/")
     public String index(Model model)
     {
-
         model.addAttribute("colorlist",colorList.findAll());
         model.addAttribute("ownerlist",colorOwners.findAll());
         return "index";
     }
 
     @RequestMapping("/addcolor")
-    public String addPet(Model model)
+    public String addPersonColorInfo(Model model)
     {
         model.addAttribute("person", new Person());
         model.addAttribute("colorlist",colorList.findAll());
         return "color";
     }
 
-    @RequestMapping("/savecolor")
-    public String savePet(@ModelAttribute("person") Person person, Model model)
+    @PostMapping("/savecolor")
+    public String savePersonColorInfo(@ModelAttribute("person") Person person)
     {
         colorOwners.save(person);
         return "redirect:/";
     }
+
+/*  @RequestMapping("/updatecolorlist")
+    public void updateColorList()
+    {
+        Color c;
+        c = new Color();
+        c.setColorName("");
+        colorList.save(c);
+    }*/
 
     @PostConstruct
     public void fillTables()
@@ -50,11 +60,27 @@ public class HomeController {
         colorList.save(c);
 
         c = new Color();
+        c.setColorName("orange");
+        colorList.save(c);
+
+        c = new Color();
+        c.setColorName("yellow");
+        colorList.save(c);
+
+        c = new Color();
+        c.setColorName("green");
+        colorList.save(c);
+
+        c = new Color();
         c.setColorName("blue");
         colorList.save(c);
 
         c = new Color();
-        c.setColorName("orange");
+        c.setColorName("indigo");
+        colorList.save(c);
+
+        c = new Color();
+        c.setColorName("violet");
         colorList.save(c);
     }
 }
